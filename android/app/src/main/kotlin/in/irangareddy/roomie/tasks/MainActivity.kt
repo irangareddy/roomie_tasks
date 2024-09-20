1c5f1db27f6a0334b9a158fd221838cfb1ff3c4c
Ranga Reddy Nukala <60821111+irangareddy@users.noreply.github.com>
2024-09-20 10:45:34 -0700

    project setup

---
+++ android/app/src/main/kotlin/in/irangareddy/roomie/tasks/MainActivity.kt
@@ -0,0 +1,6 @@
+package in.irangareddy.roomie_tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:roomie_tasks/app/app.dart';
+import 'package:roomie_tasks/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
